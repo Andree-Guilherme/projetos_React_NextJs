c180955d13d8c5b2d1bd12a3686934a079fff228
dev-andree <164929880+Andree-Guilherme@users.noreply.github.com>
2025-10-31 10:54:22 -0300

    Utilizando componentes aninhados
    Módulo: 02 Começando com ReactJS
    Aula: 03 Propriedades e componentes
    
    https://github.com/Andree-Guilherme/curso_React_NextJS/blob/draft/02%20Come%C3%A7ando%20com%20ReactJS/03%20Propriedades%20e%20componente/Componentes.md

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -2,8 +2,17 @@ function App() {
   return (
     <div>
       <h1>Bem - vindo ao meu primeiro App com React!</h1>
+      <Nome />
     </div>
   );
 }
 
 export default App;
+
+function Nome(){
+  return(
+    <span>
+      Bem vindo: Usuário
+    </span>
+  );
+}
