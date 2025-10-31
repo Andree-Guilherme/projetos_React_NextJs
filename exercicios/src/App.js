33d0e19afcc96a4a64073c77dd7a5f188fe567aa
dev-andree <164929880+Andree-Guilherme@users.noreply.github.com>
2025-10-31 13:28:10 -0300

    Passando argumentos para a função de atualização
    Módulo: 02 Começando com ReactJS
    Aula: 04 Trabalhando com useState
    
    https://github.com/Andree-Guilherme/curso_React_NextJS/tree/draft/02%20Come%C3%A7ando%20com%20ReactJS/04%20Trabalhando%20com%20useState

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -3,15 +3,15 @@ import { useState } from 'react'
 function App() {
   const [aluno, setAluno] = useState('André')
 
-  function handleChangeName() {
-  setAluno('Matheus');
+  function handleChangeName(nome) {
+  setAluno(nome);
 }
   
   return (
     <div>
         <h1>Bem-vindo ao meu primeiro App com React!</h1>
         <h2>Olá: {aluno}</h2>
-        <button onClick={handleChangeName}>
+        <button onClick={ () => handleChangeName('Guilherme') }>
           Mudar nome
         </button>
       </div>
